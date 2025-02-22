28d294a604a161e2e6a6f93b7b4cf4c097953046
Нороха Нестор Тарасович <ntnorokha@edu.hse.ru>
2025-02-22 13:49:01 +0300

    Добавлено убирание сайдбара при адаптиве

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
+const adaptBtn = document.querySelector('#adaptLink');
+const adaptBtn1 = document.querySelector('#adaptBtn1');
+const adaptBtn2 = document.querySelector('#adaptBtn2');
+const adaptBtn3 = document.querySelector('#adaptBtn3');
 
 
 navBtn.onclick = ()=>{
@@ -9,10 +13,25 @@ navBtn.onclick = ()=>{
         navBtnImg.src= "./assets/svg/utils/close.svg"
     }
     else{
-        navBtnImg.src= "/assets/svg/utils/NAV.svg"
+        navBtnImg.src= "./assets/svg/utils/NAV.svg"
     }
+
+}
+console.table(adaptBtn)
+
+adaptBtn.onclick = ()=>{
+    navBtn.onclick();
 }
 
+adaptBtn1.onclick = ()=>{
+    navBtn.onclick();
+}
+adaptBtn2.onclick = ()=>{
+    navBtn.onclick();
+}
+adaptBtn3.onclick = ()=>{
+    navBtn.onclick();
+}
 AOS.init({
     once: true,
 });
